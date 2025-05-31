fbc2deb50e9e278848f3901847c3d6309653d8ad
Waihon Yew <yewwaihon@gmail.com>
2025-05-31 13:00:42 +0800

    Initial version generated by Spring Initializr

---
+++ src/test/java/com/waihon/cashcard/CashCardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waihon.cashcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashCardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
