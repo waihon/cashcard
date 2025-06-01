b40cf388e7f1fcaca05e2dbd3644fe933f455c62
Waihon Yew <yewwaihon@gmail.com>
2025-06-01 18:14:26 +0800

    Use @PathVariable for recognizing requestedId

--- src/main/java/com/waihon/cashcard/controller/CashCardController.java
+++ src/main/java/com/waihon/cashcard/controller/CashCardController.java
@@ -3,6 +3,7 @@ package com.waihon.cashcard.controller;
 import com.waihon.cashcard.entity.CashCard;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,8 +18,14 @@ class CashCardController {
     // @GetMapping marks a method as a handler method. GET requests that matches
     // cashcards/{requestedId} will be handled by this method.
     @GetMapping("/{requestedId}")
-    private ResponseEntity<CashCard> findById() {
-        CashCard cashCard = new CashCard(99L, 123.45);
-        return ResponseEntity.ok(cashCard);
+    // @PathVariable makes Spring Web aware of the requestedId supplied in the HTTP request.
+    // Now it's available for us to use in our handler method.
+    private ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
+        if (requestedId.equals(99L)) {
+            CashCard cashCard = new CashCard(99L, 123.45);
+            return ResponseEntity.ok(cashCard);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
