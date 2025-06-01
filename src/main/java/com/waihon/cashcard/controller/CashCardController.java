20a42531a905a8f1047ecb6cb17ba1fa54bda7b1
Waihon Yew <yewwaihon@gmail.com>
2025-06-01 17:21:33 +0800

    Add the GET endpoint

--- src/main/java/com/waihon/cashcard/controller/CashCardController.java
+++ src/main/java/com/waihon/cashcard/controller/CashCardController.java
@@ -1,9 +1,21 @@
 package com.waihon.cashcard.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+// Tells Spring that this class is a Component of type RestController and capable of
+// handling HTTP requests.
+@RestController
+// A companion to @RestController that indicates which address requests must have
+// to access this Controller.
+@RequestMapping("/cashcards")
 class CashCardController {
 
+    // @GetMapping marks a method as a handler method. GET requests that matches
+    // cashcards/{requestedId} will be handled by this method.
+    @GetMapping("/{requestedId}")
     private ResponseEntity<String> findById() {
         return ResponseEntity.ok("{}");
     }
