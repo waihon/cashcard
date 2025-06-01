b84c4b7e08d0d83876ad63a3c1c1458be8829a1b
Waihon Yew <yewwaihon@gmail.com>
2025-06-01 17:50:25 +0800

    Move CashCard to entity and make it publicly available

--- src/main/java/com/waihon/cashcard/CashCard.java
+++
@@ -1,4 +0,0 @@
-package com.waihon.cashcard;
-
-record CashCard(Long id, Double amount) {
-}
---
+++ src/main/java/com/waihon/cashcard/entity/CashCard.java
@@ -0,0 +1,4 @@
+package com.waihon.cashcard.entity;
+
+public record CashCard(Long id, Double amount) {
+}
