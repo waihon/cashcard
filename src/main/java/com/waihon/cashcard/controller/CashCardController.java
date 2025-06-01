d2ae9dbf69cf2f01835f706c0c4f8e3dc0416d77
Waihon Yew <yewwaihon@gmail.com>
2025-06-01 17:17:27 +0800

    Create a REST controller

---
+++ src/main/java/com/waihon/cashcard/controller/CashCardController.java
@@ -0,0 +1,10 @@
+package com.waihon.cashcard.controller;
+
+import org.springframework.http.ResponseEntity;
+
+class CashCardController {
+
+    private ResponseEntity<String> findById() {
+        return ResponseEntity.ok("{}");
+    }
+}
