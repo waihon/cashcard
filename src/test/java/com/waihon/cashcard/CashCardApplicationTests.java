42a19107624f3dfdc1dd3cd60114e00e61455c9d
Waihon Yew <yewwaihon@gmail.com>
2025-06-01 18:00:44 +0800

    Write a test for cash card with an unknown ID

--- src/test/java/com/waihon/cashcard/CashCardApplicationTests.java
+++ src/test/java/com/waihon/cashcard/CashCardApplicationTests.java
@@ -40,4 +40,12 @@ class CashCardApplicationTests {
 		assertThat(amount).isEqualTo(123.45);
 	}
 
+	@Test
+	void shouldNotReturnACashCardWithAnUnknownId() {
+		ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/1000", String.class);
+
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+		assertThat(response.getBody()).isBlank();
+	}
+
 }
