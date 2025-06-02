dfd4b16197e6255dc39ceef8438dfd1553b8d05d
Waihon Yew <yewwaihon@gmail.com>
2025-06-02 22:24:23 +0800

    Use CashCardRepository for data management

--- src/main/java/com/waihon/cashcard/controller/CashCardController.java
+++ src/main/java/com/waihon/cashcard/controller/CashCardController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 // Tells Spring that this class is a Component of type RestController and capable of
 // handling HTTP requests.
 @RestController
@@ -28,9 +30,9 @@ class CashCardController {
     // @PathVariable makes Spring Web aware of the requestedId supplied in the HTTP request.
     // Now it's available for us to use in our handler method.
     private ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
-        if (requestedId.equals(99L)) {
-            CashCard cashCard = new CashCard(99L, 123.45);
-            return ResponseEntity.ok(cashCard);
+        Optional<CashCard> cashCardOptional = cashCardRepository.findById(requestedId);
+        if (cashCardOptional.isPresent()) {
+            return ResponseEntity.ok(cashCardOptional.get());
         } else {
             return ResponseEntity.notFound().build();
         }
