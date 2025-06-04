10a2aca95f762c92b7cd8d84eb7c5bee0172cf6e
Waihon Yew <yewwaihon@gmail.com>
2025-06-04 13:32:01 +0800

    Test the HTTP POST endpoint

--- src/test/java/com/waihon/cashcard/CashCardApplicationTests.java
+++ src/test/java/com/waihon/cashcard/CashCardApplicationTests.java
@@ -2,6 +2,7 @@ package com.waihon.cashcard;
 
 import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
+import com.waihon.cashcard.entity.CashCard;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -46,6 +47,16 @@ class CashCardApplicationTests {
 
 		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
 		assertThat(response.getBody()).isBlank();
+	}
+
+	@Test
+	void shouldCreateANewCashCard() {
+		// The database will create and manage all unique CashCard.id values for us. We shouldn't provide one.
+		// Besides, we must also provide newCashCard data for the new CashCard.
+		CashCard newCashCard = new CashCard(null, 250.00);
+		// In addition, we don't expect a CashCard to be returned to us, so we expect a Void response body.
+		ResponseEntity<Void> createResponse = restTemplate.postForEntity("/cashcards", newCashCard, Void.class);
+		assertThat(createResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
 	}
 
 }
