0bb42c1ca8e8e489d453e68a16edb3ed58875453
Waihon Yew <yewwaihon@gmail.com>
2025-06-04 20:36:16 +0800

    Add @DirtiesContext to CashCardApplicationTests

--- src/test/java/com/waihon/cashcard/CashCardApplicationTests.java
+++ src/test/java/com/waihon/cashcard/CashCardApplicationTests.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.DirtiesContext;
 
 import java.net.URI;
 
@@ -16,6 +17,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 // Start Spring Boot application and make it available for our test to perform requests to it.
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class CashCardApplicationTests {
 
 	// Ask Spring to inject a test helper that'll allow us to make HTTP requests to the
