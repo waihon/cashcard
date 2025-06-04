d6a8230d167306dce86391d67fbb55c6dc89c270
Waihon Yew <yewwaihon@gmail.com>
2025-06-04 16:57:41 +0800

    Implement the POST endpoint

--- src/main/java/com/waihon/cashcard/controller/CashCardController.java
+++ src/main/java/com/waihon/cashcard/controller/CashCardController.java
@@ -4,7 +4,9 @@ import com.waihon.cashcard.entity.CashCard;
 import com.waihon.cashcard.repository.CashCardRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.Optional;
 
 // Tells Spring that this class is a Component of type RestController and capable of
@@ -36,8 +38,21 @@ class CashCardController {
     }
 
     @PostMapping
-    private ResponseEntity<Void> createCashCard() {
-        // By returning nothing at all, Spring Web will automatically generate an HTTP Response Status code of 200 OK.
-        return null;
+    // POST expects a request "body". This contains the data submitted to the API.
+    // Spring Web will deserialize the data into a CashCard for us.
+    // UriComponentsBuilder is automatically injected from Spring's IoC Container.
+    private ResponseEntity<Void> createCashCard(@RequestBody CashCard newCashCardRequest,
+                                                UriComponentsBuilder ucb) {
+        // The save method saves a new CashCard for us, and returns the saved object
+        // with a unique id provided by the database.
+        CashCard savedCashCard = cashCardRepository.save(newCashCardRequest);
+        // This is constructing a URI to the newly created CashCard. This is the URI
+        // that the caller can then use to GET the newly-created CashCard.
+        URI locationOfNewCashCard = ucb
+                .path("cashcards/{id}")
+                .buildAndExpand(savedCashCard.id())
+                .toUri();
+        // Returns 201 CREATED with the correct Location header
+        return ResponseEntity.created(locationOfNewCashCard).build();
     }
 }
