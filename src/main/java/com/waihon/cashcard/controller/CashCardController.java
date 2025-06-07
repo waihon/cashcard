fa40ba6e395b09de8ccfea9c5da2083a50430c93
Waihon Yew <yewwaihon@gmail.com>
2025-06-07 09:02:04 +0800

    Implement paging and sorting defaults

--- src/main/java/com/waihon/cashcard/controller/CashCardController.java
+++ src/main/java/com/waihon/cashcard/controller/CashCardController.java
@@ -6,6 +6,7 @@ import org.apache.coyote.Response;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -72,7 +73,9 @@ class CashCardController {
                 PageRequest.of(
                         pageable.getPageNumber(),
                         pageable.getPageSize(),
-                        pageable.getSort()
+                        // getSortOr() method provides default values for the page (0), size (20),
+                        // and sort parameters.
+                        pageable.getSortOr(Sort.by(Sort.Direction.ASC, "amount"))
                 )
         );
         return ResponseEntity.ok(page.getContent());
