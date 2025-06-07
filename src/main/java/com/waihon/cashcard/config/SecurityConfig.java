de030504275d08c24547a9acb0b065858e973735
Waihon Yew <yewwaihon@gmail.com>
2025-06-07 18:18:01 +0800

    Satisfy Spring Security's dependencies

---
+++ src/main/java/com/waihon/cashcard/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.waihon.cashcard.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+// Tells Spring to use this class to configure Spring and Spring Boot itself.
+@Configuration
+class SecurityConfig {
+
+    // Spring Security expects a Bean to configure its Filter Chain. Annotating a method
+    // return a SecurityFilterChain with the @Bean satisfies this expectation.
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // Minimum needed for now
+        return http.build();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
