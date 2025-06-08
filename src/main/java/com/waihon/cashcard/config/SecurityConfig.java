0d8835a09a8046d4b6f3709e56454c59c28fd9da
Waihon Yew <yewwaihon@gmail.com>
2025-06-08 09:24:58 +0800

    Add users and roles to the UserDetailsService Bean

--- src/main/java/com/waihon/cashcard/config/SecurityConfig.java
+++ src/main/java/com/waihon/cashcard/config/SecurityConfig.java
@@ -44,8 +44,13 @@ class SecurityConfig {
         UserDetails sarah = user
                 .username("sarah1")
                 .password(passwordEncoder.encode("abc123"))
-                .roles() // No roles for now
+                .roles("CARD-OWNER") // new role
                 .build();
-        return new InMemoryUserDetailsManager(sarah);
+        UserDetails hankOwnsNoCards = user
+                .username("hank-owns-no-cards")
+                .password(passwordEncoder.encode("qrs456"))
+                .roles("NON-OWNER") // new role
+                .build();
+        return new InMemoryUserDetailsManager(sarah, hankOwnsNoCards);
     }
 }
