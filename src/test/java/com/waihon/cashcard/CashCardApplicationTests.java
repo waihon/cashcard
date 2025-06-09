a66500187733ac1bcae8d14eb2f1d274308d0aa4
Waihon Yew <yewwaihon@gmail.com>
2025-06-09 22:24:23 +0800

    Write the Update test first

--- src/test/java/com/waihon/cashcard/CashCardApplicationTests.java
+++ src/test/java/com/waihon/cashcard/CashCardApplicationTests.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.DirtiesContext;
@@ -177,6 +179,18 @@ class CashCardApplicationTests {
 				.withBasicAuth("sarah1", "abc123")
 				.getForEntity("/cashcards/102", String.class);
 		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+	}
+
+	@Test
+	void shouldUpdateAnExistingCashCard() {
+		CashCard cashCardUpdate = new CashCard(null, 19.99, null);
+		// Create the HttpEntity that the exchange() method needs.
+		HttpEntity<CashCard> request = new HttpEntity<>(cashCardUpdate);
+		ResponseEntity<Void> response = restTemplate
+				.withBasicAuth("sarah1", "abc123")
+				// exchange() method is used because putForEntity() doesn't exist.
+				.exchange("/cashcards/99", HttpMethod.PUT, request, Void.class);
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
 	}
 
 }
