bed247c983968b1836d5b4b9d5027e8f3582f749
Waihon Yew <yewwaihon@gmail.com>
2025-06-10 01:08:30 +0800

    Update CashCardController to perform data update

--- src/main/java/com/waihon/cashcard/controller/CashCardController.java
+++ src/main/java/com/waihon/cashcard/controller/CashCardController.java
@@ -86,8 +86,14 @@ class CashCardController {
     }
 
     @PutMapping("/{requestedId}")
-    private ResponseEntity<Void> putCashCard(@PathVariable Long requestedId, @RequestBody CashCard cashCardUpdate) {
-        // Just return 204 NO CONTENT for now.
+    // Add the Principal as a method argument, provided automatically by Spring Security.
+    private ResponseEntity<Void> putCashCard(@PathVariable Long requestedId, @RequestBody CashCard cashCardUpdate, Principal principal) {
+        // Scope our retrieval of the CashCard to the submitted requestedId and Principal to ensure only the authenticated,
+        // authorized owner may update this CashCard.
+        CashCard cashCard = cashCardRepository.findByIdAndOwner(requestedId, principal.getName());
+        // Build a CashCard with updated values and save it.
+        CashCard updatedCashCard = new CashCard(cashCard.id(), cashCardUpdate.amount(), principal.getName());
+        cashCardRepository.save(updatedCashCard);
         return ResponseEntity.noContent().build();
     }
 
