245efedc1d87c8b409fb262e6b21e2f103bba583
Waihon Yew <yewwaihon@gmail.com>
2025-06-10 01:16:30 +0800

    Test should not update a CashCard that doesn't exist

--- src/test/java/com/waihon/cashcard/CashCardApplicationTests.java
+++ src/test/java/com/waihon/cashcard/CashCardApplicationTests.java
@@ -204,4 +204,14 @@ class CashCardApplicationTests {
 		assertThat(amount).isEqualTo(19.99);
 	}
 
+	@Test
+	void shouldNotUpdateACashCardThatDoesNotExist() {
+		CashCard unknownCard = new CashCard(null, 19.99, null);
+		HttpEntity<CashCard> request = new HttpEntity<>(unknownCard);
+		ResponseEntity<Void> response = restTemplate
+				.withBasicAuth("sarah1", "abc123")
+				.exchange("/cashcards/99999", HttpMethod.PUT, request, Void.class);
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+	}
+
 }
