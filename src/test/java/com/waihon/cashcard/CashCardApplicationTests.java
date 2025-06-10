e5cbeab785988560f2c1428106611d2ac8dc0705
Waihon Yew <yewwaihon@gmail.com>
2025-06-10 22:20:34 +0800

    Test that should not delete a CashCard that doesn't exist

--- src/test/java/com/waihon/cashcard/CashCardApplicationTests.java
+++ src/test/java/com/waihon/cashcard/CashCardApplicationTests.java
@@ -249,4 +249,11 @@ class CashCardApplicationTests {
 		assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
 	}
 
+	@Test
+	void shouldNotDeleteACashCardThatDoesNotExist() {
+		ResponseEntity<Void> deleteResponse = restTemplate
+				.withBasicAuth("sarah1", "abc123")
+				.exchange("/cashcards/99999", HttpMethod.DELETE, null, Void.class);
+		assertThat(deleteResponse.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+	}
 }
